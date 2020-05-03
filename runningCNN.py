58b60bac1fe470d18fea2c6d8f121d83172352dc
eden-kr <kzx789@naver.com>
2020-05-03 13:17:42 +0900

    code

---
+++ runningCNN.py
@@ -0,0 +1,145 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 12 20:29:49 2019
+
+@author: kzx789
+"""
+
+from PIL import Image
+import os, glob, numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import cv2
+import pymysql
+import MySQLdb as mysql
+
+"""
+#csv를 읽어서 영양정보 출력
+def get_Nutrition(str) :
+     nutrition = pd.read_csv('C:/식품영양정보/영양정보.csv')   
+     print(nutrition[nutrition['음식명'] == str])
+"""     
+#사용된 전체 이미지 출력
+def drawing_plt():
+    thisImg = os.listdir(caltech_dir)
+    row = 4
+    cols = int(math.ceil(len(thisImg)/4))    #반올림
+    fig = plt.figure()
+    i = 1
+        
+    for image in glob.glob("C:/cnnTest/*.jpg"):     #glob를 사용해서 Test로 사용된 파일 가져오기
+        img = cv2.imread(image)
+        subplot = fig.add_subplot(row, cols, i)
+        subplot.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))    #기본컬러
+        subplot.set_title(thisImg[i-1])     #타이틀 붙이기
+        subplot.axis("off") 
+        i += 1
+    print('\t',"전체 이미지 리스트   ")
+    plt.show()
+
+#조건에 맞는 개별 이미지 출력
+def get_Image(str):
+    imgPath = 'C:/cnnTest/'
+    image = cv2.imread(imgPath+str)
+    image = cv2.cvtColor(image,cv2.COLOR_BGR2RGB)
+    plt.imshow(image)
+    plt.xticks([])
+    plt.yticks([])
+    plt.show()
+
+#데이터베이스에서 영양소 정보 가지고 오기
+def get_DB_Nutrition(str):
+    db = pymysql.connect(host="localhost", user = "yeha", password="", db="nutrition")
+    cur = db.cursor()        #Connection에서 Cursor생성
+    sql = "SELECT * FROM NUTRITION_INFO WHERE FOODNAME LIKE '음식명' OR FOODNAME LIKE %s"
+    cur.execute(sql,(str))
+    data = cur.fetchall()    #정보 전부 가져오기
+    df = pd.Series(data[0],data[1])
+    print(df)
+    db.close()
+
+
+caltech_dir = "C:/cnnTest"
+
+#테스트할 데이터들을  128*128로 지정
+image_w = 128
+image_h = 128
+pixels = image_h * image_w * 3      #픽셀 지정
+
+X = []
+#filenames = []
+
+files = os.listdir(caltech_dir)     #하위 디렉터리 파일 리스트 구하기
+
+#print(files)                       #이미지 목록 확인 
+
+for i in range(len(files)):
+    files[i]=caltech_dir+'/'+ files[i]
+#print(files)  
+
+for f in files:
+    img = Image.open(f)
+    img = img.convert("RGB")
+    img = img.resize((image_w, image_h))
+    data = np.asarray(img)
+   # filenames.append(f)
+    X.append(data)
+
+X = np.array(X)
+#print(X)
+
+#모델 불러오기
+from keras.models import load_model
+
+model = load_model("C:/image/train/model/multi_img_classification.model")
+prediction = model.predict(X)
+#print(prediction)
+
+np.set_printoptions(formatter={'float': lambda x: "{0:0.3f}".format(x)})
+
+
+print('프로그램을 실행합니다..')
+print('\n')
+thisImg = os.listdir(caltech_dir)
+cnt = 0
+
+for i in prediction:
+    pre_ans = i.argmax()  # 예측 레이블//가장 큰 번째 수
+    #print(i)
+    #print(pre_ans)
+    pre_ans_str = ''
+    if pre_ans == 0: pre_ans_str = "연어회"
+    elif pre_ans == 1: pre_ans_str = "쌀국수"
+    elif pre_ans == 2: pre_ans_str = "샌드위치"
+    else: pre_ans_str = "새우튀김"
+
+    if i[0] >= 0.8 : 
+        get_Image(thisImg[cnt])
+        print(thisImg[cnt]+" 이미지는 "+pre_ans_str+"(으)로 추정됩니다.")
+        #get_Nutrition(pre_ans_str) 
+        get_DB_Nutrition(pre_ans_str)
+
+    if i[1] >= 0.8: 
+        get_Image(thisImg[cnt])
+        print(thisImg[cnt]+" 이미지는 "+pre_ans_str+"(으)로 추정됩니다.")
+        #get_Nutrition(pre_ans_str)   
+        get_DB_Nutrition(pre_ans_str)
+
+
+    if i[2] >= 0.8: 
+        get_Image(thisImg[cnt])
+        print(thisImg[cnt]+" 이미지는 "+pre_ans_str+"(으)로 추정됩니다.")
+        #get_Nutrition(pre_ans_str)   
+        get_DB_Nutrition(pre_ans_str)
+
+    if i[3] >= 0.8: 
+        get_Image(thisImg[cnt])
+        print(thisImg[cnt]+" 이미지는 "+pre_ans_str+"(으)로 추정됩니다.")
+        #get_Nutrition(pre_ans_str)   
+        get_DB_Nutrition(pre_ans_str)
+    cnt += 1
+    
+drawing_plt()
+
+            
